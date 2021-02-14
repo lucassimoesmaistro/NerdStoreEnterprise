5eee2ca46f96d44189ae99391c60a685f4727e2f
Lucas <lucassimoes_@hotmail.com>
2021-02-14 17:38:33 -0300

    Configurando o JWT na API

---
+++ src/services/NSE.Identidade.Api/Extensions/AppSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace NSE.Identidade.Api.Extensions
+{
+    public class AppSettings
+    {
+
+        public string Secret { get; set; }
+        public int ExpiracaoHoras { get; set; }
+        public string Emissor { get; set; }
+        public string ValidoEm { get; set; }
+    }
+}
