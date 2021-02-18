4b5a342bf216ca15dc048199bda11cf0d848ea42
Lucas <lucassimoes_@hotmail.com>
2021-02-18 07:11:39 -0300

    Catalogo HttpService

--- src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -11,6 +11,14 @@ namespace NSE.WebApp.MVC.Configuration
         {
             services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
 
+            services.AddHttpClient<ICatalogoService, CatalogoService>();
+                //.AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
+                //.AddTransientHttpErrorPolicy(
+                //p => p.WaitAndRetryAsync(3, _ => TimeSpan.FromMilliseconds(600)));
+                //.AddPolicyHandler(PollyExtensions.EsperarTentar())
+                //.AddTransientHttpErrorPolicy(
+                    //p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
+
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IUser, AspNetUser>();
         }
