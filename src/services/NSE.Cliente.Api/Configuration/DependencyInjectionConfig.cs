158ae06e56acc042ad455f56b4e32713c97d00d1
Lucas <lucassimoes_@hotmail.com>
2021-03-15 06:31:55 -0300

    Mapeamento das tabelas

---
+++ src/services/NSE.Cliente.Api/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NSE.Clientes.Api.Data;
+using NSE.Clientes.Api.Data.Repository;
+using NSE.Clientes.Api.Models;
+
+namespace NSE.Clientes.Api.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            //services.AddScoped<IMediatorHandler, MediatorHandler>();
+            //services.AddScoped<IRequestHandler<RegistrarClienteCommand, ValidationResult>, ClienteCommandHandler>();
+
+            //services.AddScoped<INotificationHandler<ClienteRegistradoEvent>, ClienteEventHandler>();
+
+            services.AddScoped<IClienteRepository, ClienteRepository>();
+            services.AddScoped<ClientesContext>();
+        }
+    }
+}
