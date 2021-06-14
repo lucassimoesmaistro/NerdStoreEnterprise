de731edf587acf6af027094e0a2089d45b10fced
Lucas <lucassimoes_@hotmail.com>
2021-06-14 07:01:07 -0300

    Aplicando o voucher via BFF Compras

--- src/services/NSE.Pedido.Api/Controllers/VoucherController.cs
+++ src/services/NSE.Pedido.Api/Controllers/VoucherController.cs
@@ -1,10 +1,33 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using NSE.WebAPI.Core.Controllers;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NSE.Pedido.Api.Application.Queries;
+using NSE.Pedido.Api.Application.DTO;
 
 namespace NSE.Pedido.Api.Controllers
 {
     [Authorize]
     public class VoucherController : MainController
     {
+        private readonly IVoucherQueries _voucherQueries;
+
+        public VoucherController(IVoucherQueries voucherQueries)
+        {
+            _voucherQueries = voucherQueries;
+        }
+
+        [HttpGet("voucher/{codigo}")]
+        [ProducesResponseType(typeof(VoucherDTO), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<IActionResult> ObterPorCodigo(string codigo)
+        {
+            if (string.IsNullOrEmpty(codigo)) return NotFound();
+
+            var voucher = await _voucherQueries.ObterVoucherPorCodigo(codigo);
+
+            return voucher == null ? NotFound() : CustomResponse(voucher);
+        }
     }
 }
