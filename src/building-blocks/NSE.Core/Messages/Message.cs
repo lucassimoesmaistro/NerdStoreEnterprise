1f541f6b90cbad2dc35467b49dd0e7a62aa772a9
Lucas <lucassimoes_@hotmail.com>
2021-07-15 11:34:17 -0300

    Entendendo o Gateway de pagamento

--- src/building-blocks/NSE.Core/Messages/Message.cs
+++ src/building-blocks/NSE.Core/Messages/Message.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace NSE.Core.Messages
 {
-    // docker run -d --hostname rabbit-host --name rabbit-nerdstore -p 15672:15672 -p 5672:5672 rabbitmq:managment
     public abstract class Message
     {
         public string MessageType { get; protected set; }
---
+++ src/services/NSE.Pagamento.Api/Models/TipoPagamento.cs
@@ -0,0 +1,8 @@
+﻿namespace NSE.Pagamentos.Api.Models
+{
+    public enum TipoPagamento
+    {
+        CartaoCredito = 1,
+        Boleto
+    }
+}
