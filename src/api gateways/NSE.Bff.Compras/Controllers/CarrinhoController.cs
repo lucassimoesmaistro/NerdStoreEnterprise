aa32b4f0afa5c60459aa667f2821e9991176d7b0
Lucas <lucassimoes_@hotmail.com>
2021-07-21 10:56:43 -0300

    Implementando um gRPC Client

--- src/api gateways/NSE.Bff.Compras/Controllers/CarrinhoController.cs
+++ src/api gateways/NSE.Bff.Compras/Controllers/CarrinhoController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using NSE.Bff.Compras.Models;
 using NSE.Bff.Compras.Services;
+using NSE.Bff.Compras.Services.gRPC;
 using NSE.WebAPI.Core.Controllers;
 
 namespace NSE.Bff.Compras.Controllers
@@ -14,15 +15,18 @@ namespace NSE.Bff.Compras.Controllers
     public class CarrinhoController : MainController
     {
         private readonly ICarrinhoService _carrinhoService;
+        private readonly ICarrinhoGrpcService _carrinhoGrpcService;
         private readonly ICatalogoService _catalogoService;
         private readonly IPedidoService _pedidoService;
 
         public CarrinhoController(
             ICarrinhoService carrinhoService,
+            ICarrinhoGrpcService carrinhoGrpcService,
             ICatalogoService catalogoService,
             IPedidoService pedidoService)
         {
             _carrinhoService = carrinhoService;
+            _carrinhoGrpcService = carrinhoGrpcService;
             _catalogoService = catalogoService;
             _pedidoService = pedidoService;
         }
@@ -31,14 +35,14 @@ namespace NSE.Bff.Compras.Controllers
         [Route("compras/carrinho")]
         public async Task<IActionResult> Index()
         {
-            return CustomResponse(await _carrinhoService.ObterCarrinho());
+            return CustomResponse(await _carrinhoGrpcService.ObterCarrinho());
         }
 
         [HttpGet]
         [Route("compras/carrinho-quantidade")]
         public async Task<int> ObterQuantidadeCarrinho()
         {
-            var quantidade = await _carrinhoService.ObterCarrinho();
+            var quantidade = await _carrinhoGrpcService.ObterCarrinho();
             return quantidade?.Itens.Sum(i => i.Quantidade) ?? 0;
         }
 
